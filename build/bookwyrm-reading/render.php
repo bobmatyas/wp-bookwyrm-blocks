f46a9708cf69179c0596f6495f502cd1351d4391
bob <45246438+bobmatyas@users.noreply.github.com>
2024-07-07 13:56:41 -0400

    fixes from plugin check

--- build/bookwyrm-reading/render.php
+++ build/bookwyrm-reading/render.php
@@ -1,3 +1,3 @@
-<div <?php echo get_block_wrapper_attributes(); ?> data-user=<? echo esc_attr( $attributes['bookwyrmUserName'] ) ?> data-instance=<? echo esc_attr( $attributes['bookwyrmInstance'] ) ?>>
+<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?> data-user=<?php echo esc_attr( $attributes['bookwyrmUserName'] ); ?> data-instance=<?php echo esc_attr( $attributes['bookwyrmInstance'] ); ?>>
 	<div class="reading--list"></div>
 </div>
